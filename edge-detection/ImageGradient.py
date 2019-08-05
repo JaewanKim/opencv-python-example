9823e2b31c82367856340ab6c4c7c859a826fdc1
JaewanKim <wodhks6561@gmail.com>
2019-08-06 06:06:35 +0900

    [UPDATE] Apply Code Review

--- edge-detection/ImageGradient.py
+++ edge-detection/ImageGradient.py
@@ -1,7 +1,7 @@
 import cv2
 from matplotlib import pyplot as plt
 import numpy as np
-
+import math
 
 img = './Image/lenna.png'
 
@@ -23,42 +23,58 @@ for h in range(1, height - 1):
     for w in range(1, width - 1):
 
         # Calculate simple brightness differences in the x-axis & y-axis direction
-        brightness_diff_x = 255 - original_img[h, w + 1] + original_img[h, w - 1]
-        brightness_diff_y = 255 - original_img[h + 1, w] + original_img[h - 1, w]
+        brightness_diff_x = original_img[h, w + 1] - original_img[h, w - 1]
+        brightness_diff_y = original_img[h + 1, w] - original_img[h - 1, w]
 
-        img_gradient_diff_x.itemset(h, w, brightness_diff_x)
-        img_gradient_diff_y.itemset(h, w, brightness_diff_y)
+        img_gradient_diff_x.itemset(h, w, 128 + brightness_diff_x)
+        img_gradient_diff_y.itemset(h, w, 128 + brightness_diff_y)
 
         # Matrix operation using sobel filter (3 * 3)
         # Get pixel values from the original image for each axis
-        local_matrix_x = 255 - np.matrix([
+        local_matrix_x = np.matrix([
                                 [original_img[h - 1, w - 1], original_img[h, w - 1], original_img[h + 1, w - 1]],
                                 [original_img[h - 1, w], original_img[h, w], original_img[h + 1, w + 1]],
                                 [original_img[h - 1, w + 1], original_img[h + 1, w + 1], original_img[h + 1, w + 1]]])
 
-        local_matrix_y = 255 - np.matrix([
+        local_matrix_y = np.matrix([
                                 [original_img[h - 1, w - 1], original_img[h - 1, w], original_img[h - 1, w + 1]],
                                 [original_img[h, w - 1], original_img[h, w], original_img[h, w + 1]],
                                 [original_img[h + 1, w - 1], original_img[h + 1, w], original_img[h + 1, w + 1]]])
 
-        local_matrix = local_matrix_x + local_matrix_y
-
         # Matrix operation
         sobel_gx_local_matrix = np.dot(sobel_filter, local_matrix_x)
         sobel_gy_local_matrix = np.dot(sobel_filter, local_matrix_y)
-        sobel_gxy_local_matrix = np.dot(sobel_filter, local_matrix)
 
         # Save Image Gradient by pixel
         for i in range(h - 1, h + 1):
             for j in range(w - 1, w + 1):
-                img_gradient_sobel_x.itemset(i, j, sobel_gx_local_matrix[i - h + 1, j - w + 1])
-                img_gradient_sobel_y.itemset(i, j, sobel_gy_local_matrix[i - h + 1, j - w + 1])
-                img_gradient_sobel_xy.itemset(i, j, sobel_gxy_local_matrix[i - h + 1, j - w + 1])
+                img_gradient_sobel_x.itemset(i, j, 128 + sobel_gx_local_matrix[i - h + 1, j - w + 1])
+                img_gradient_sobel_y.itemset(i, j, 128 + sobel_gy_local_matrix[i - h + 1, j - w + 1])
+                img_gradient_sobel_xy.itemset(i, j, 0 + math.sqrt( math.pow(sobel_gx_local_matrix[i - h + 1, j - w + 1], 2) + math.pow(sobel_gy_local_matrix[i - h + 1, j - w + 1], 2) ))
+
+# Set pixel value greater than threshold
+threshold = 140
+for h in range(0, height):
+    for w in range(0, width):
+        if img_gradient_sobel_x[h, w] > threshold:
+            img_gradient_sobel_x.itemset(h, w, 255)
+        else:
+            img_gradient_sobel_x.itemset(h, w, 0)
+
+        if img_gradient_sobel_y[h, w] > threshold:
+            img_gradient_sobel_y.itemset(h, w, 255)
+        else:
+            img_gradient_sobel_y.itemset(h, w, 0)
+
+        # if img_gradient_sobel_xy[h, w] > 50:
+        #     img_gradient_sobel_xy.itemset(h, w, 255)
+        # else:
+        #     img_gradient_sobel_xy.itemset(h, w, 0)
 
 # UI
 images = [original_img, img_gradient_diff_x, img_gradient_diff_y,
           img_gradient_sobel_x, img_gradient_sobel_y, img_gradient_sobel_xy]
-titles = ['original', 'difference x', 'difference y', 'sobel x (3*3)', 'sobel y (3*3)', 'sobel x + y']
+titles = ['original', 'difference x', 'difference y', 'sobel x (3*3)', 'sobel y (3*3)', 'sobel xy (3*3)']
 
 for i in range(0, 6):
     plt.subplot(2, 3, i + 1), plt.imshow(images[i], cmap='gray', interpolation='bicubic'), plt.title([titles[i]])
