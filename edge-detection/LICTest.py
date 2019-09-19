6f7cf198615cdf87daa5a2f42c23a7cb0e5aadf4
JaewanKim <wodhks6561@gmail.com>
2019-09-19 18:27:24 +0900

    [UPDATE] Update LICTest.py & Create RungeKuttaTest.py (Still working)

--- edge-detection/LICTest.py
+++ edge-detection/LICTest.py
@@ -8,7 +8,8 @@ import random
 class Main:
 
     def __init__(self):
-        img = './Image/gradation3.png'
+        self.name = 'gradation3.png'
+        img = './Image/' + self.name
         self.original_img = cv2.imread(img, cv2.IMREAD_GRAYSCALE)
 
         self.height = self.original_img.shape[0]
@@ -19,30 +20,45 @@ class Main:
         self.gradient_vector_field = [[[0, 0] for col in range(self.width)] for row in range(self.height)]
         self.noise_field = [[0 for col in range(self.width)] for row in range(self.height)]
 
+    def white_noise(self):
         # Create White Noise
         for h in range(0, self.height):
             for w in range(0, self.width):
                 self.noise_field[h][w] = random.randrange(0, 256)
 
+    def image_gradient_dw(self):
+        gradient_vector_field = [[[0.0, 0.0] for col in range(self.width)] for row in range(self.height)]
+
+        for h in range(self.height):
+            for w in range(1, self.width - 1):
+                gradient_vector_field[h][w][0] = (float(self.original_img[h, w + 1]) - float(self.original_img[h, w - 1])) * 0.5
+            gradient_vector_field[h][0][0] = float(self.original_img[h, 1]) - float(self.original_img[h, 0])
+            gradient_vector_field[h][self.width - 1][0] = float(self.original_img[h, self.width - 1]) - float(self.original_img[h, self.width - 2])
+
+        for w in range(self.width):
+            for h in range(1, self.height - 1):
+                gradient_vector_field[h][w][1] = (float(self.original_img[h + 1, w]) - float(self.original_img[h - 1, w])) * 0.5
+            gradient_vector_field[0][w][1] = float(self.original_img[1, w]) - float(self.original_img[0, w])
+            gradient_vector_field[self.height - 1][w][1] = float(self.original_img[self.height - 1, w]) - float(self.original_img[self.height - 2, w])
+
+        return gradient_vector_field
+
     def image_gradient(self):
         gradient_vector_field = [[[0.0, 0.0] for col in range(self.width)] for row in range(self.height)]
 
-        for h in range(1, self.height-1):
-            for w in range(1, self.width - 1):
-                gradient_vector_field[h][w][0] = (float(self.original_img[h, w + 1]) - float(self.original_img[h, w - 1])) * 0.5
-                gradient_vector_field[h][w][1] = (float(self.original_img[h + 1, w]) - float(self.original_img[h - 1, w])) * 0.5
-
-        # for h in range(self.height):
-        #     for w in range(1, self.width - 1):
-        #         gradient_vector_field[h][w][0] = (float(self.original_img[h, w + 1]) - float(self.original_img[h, w - 1])) * 0.5
-        #     gradient_vector_field[h][0][0] = float(self.original_img[h, 1]) - float(self.original_img[h, 0])
-        #     gradient_vector_field[h][self.width - 1][0] = float(self.original_img[h, self.width - 1]) - float(self.original_img[h, self.width - 2])
-        #
-        # for w in range(self.width):
-        #     for h in range(1, self.height - 1):
-        #         gradient_vector_field[h][w][1] = (float(self.original_img[h + 1, w]) - float(self.original_img[h - 1, w])) * 0.5
-        #     gradient_vector_field[0][w][1] = float(self.original_img[1, w]) - float(self.original_img[0, w])
-        #     gradient_vector_field[self.height - 1][w][1] = float(self.original_img[self.height - 1, w]) - float(self.original_img[self.height - 2, w])
+        for h in range(0, self.height):
+            for w in range(0, self.width):
+                if h == 0:
+                    gradient_vector_field[h][w][1] = float(self.original_img[h + 1, w]) - float(self.original_img[h, w])
+                elif w == 0:
+                    gradient_vector_field[h][w][0] = float(self.original_img[h, w + 1]) - float(self.original_img[h, w])
+                elif h == (self.height - 1):
+                    gradient_vector_field[h][w][1] = float(self.original_img[h, w]) - float(self.original_img[h - 1, w])
+                elif w == (self.width - 1):
+                    gradient_vector_field[h][w][0] = float(self.original_img[h, w]) - float(self.original_img[h, w - 1])
+                else:
+                    gradient_vector_field[h][w][0] = (float(self.original_img[h, w + 1]) - float(self.original_img[h, w - 1])) * 0.5
+                    gradient_vector_field[h][w][1] = (float(self.original_img[h + 1, w]) - float(self.original_img[h - 1, w])) * 0.5
 
         return gradient_vector_field
 
@@ -95,13 +111,17 @@ class Main:
 
         return rotated_field
 
-    def __main__(self):
+    @staticmethod
+    def gaussian_weight(length, sigma=None):
+        length = int(length)
+        if sigma is None:
+            sigma = 1.0
+        w = np.mgrid[-length:length + 1]
+        g = np.exp(-(w ** 2) / (2 * (sigma ** 2))) / (sigma ** 2 * (2 * math.pi))
+        return g / g.sum()
 
-        vector_field = self.image_gradient_sobel()
-        self.gradient_vector_field = self.rotate_field(vector_field, 90.0)
+    def lic(self, length, ds):
 
-        length = 20
-        ds = 1
         for h in range(0, int(self.height)):
             for w in range(0, int(self.width)):
 
@@ -149,13 +169,72 @@ class Main:
 
                 self.result_img.itemset(h, w, int(tot))
 
+    def lic_gaussain(self, length, ds):
+        weight_list = self.gaussian_weight(length)
+
+        for h in range(0, int(self.height)):
+            for w in range(0, int(self.width)):
+
+                # array C = compute_integral_curve(p)
+                vector = self.gradient_vector_field[h][w]
+
+                x = float(w)
+                y = float(h)
+                curve_list = []
+                curve_list.append([x, y])
+
+                for s in range(1, length):
+                    x = x + ds * vector[0]
+                    y = y + ds * vector[1]
+                    if 0 <= int(x+0.5) < self.width and 0 <= int(y+0.5) < self.height:
+                        curve_list.append([x, y])
+                        vector = self.gradient_vector_field[int(y+0.5)][int(x+0.5)]
+
+                vector = self.gradient_vector_field[h][w]
+
+                for s in range(-length, 0):
+                    x = x - ds * vector[0]
+                    y = y - ds * vector[1]
+                    if 0 <= int(x+0.5) < self.width and 0 <= int(y+0.5) < self.height:
+                        curve_list.append([x, y])
+                        vector = self.gradient_vector_field[int(y+0.5)][int(x+0.5)]
+
+                # sum = compute_convolution(image, C)
+                tot: int = 0
+                tot2: int = 0
+                for i in range(0, len(curve_list)):
+                    c = curve_list[i]
+                    weight = weight_list[i]
+                    x = int(c[0] + 0.5)
+                    y = int(c[1] + 0.5)
+                    tot += (self.noise_field[y][x] * weight)
+                    tot2 += weight
+
+                if len(curve_list) != 0:
+                    tot /= tot2
+
+                self.result_img.itemset(h, w, int(tot))
+
+    def __main__(self):
+
+        self.white_noise()
+        vector_field = self.image_gradient()
+        # vector_field = self.image_gradient_sobel()
+        self.gradient_vector_field = self.rotate_field(vector_field, 90.0)
+
+        length = 20
+        ds = 1
+        # self.lic(length, ds)
+        self.lic_gaussain(length, ds)
+
+        file_name = 'lic-weightGaussian-length' + str(length) + '-ds' + str(ds) + '-' + self.name
 
         # UI
         plt.subplot()
         plt.imshow(self.result_img, cmap='gray', interpolation='bicubic')
-        plt.title('grad2 lic length20 ds5 r')
+        plt.title(file_name)
         plt.xticks([]), plt.yticks([])
-        plt.savefig('grad2-lic-length20-ds5-r.png', bbox_inches='tight')
+        plt.savefig(file_name, bbox_inches='tight')
         plt.show()
 
 
